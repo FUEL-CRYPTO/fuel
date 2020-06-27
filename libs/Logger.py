78455a240a81cd06dcc29a88a032853a9335b794
FUEL-CRYPTO <58187786+FUEL-CRYPTO@users.noreply.github.com>
2020-06-27 01:16:12 +0000

    Logger Change

--- libs/Logger.py
+++ libs/Logger.py
@@ -11,7 +11,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 # create a file handler
 handler = logging.FileHandler('blockchain.log')
